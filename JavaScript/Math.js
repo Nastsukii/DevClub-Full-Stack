a2e2dfb6c4448831280507c390309be441d029b5
Nastsukii <lacb.lzn.luiz@gamil.com>
2025-08-07 02:05:09 -0300

    adição de estudo math e um projeto sobre o mesmo

---
+++ JavaScript/Math.js
@@ -0,0 +1,17 @@
+const result = Math.pow(2,2) // Potenciação, numero elevado
+console.log(result)
+
+const raiz = Math.sqrt(25,2) // raiz quadrada 
+console.log(raiz)
+
+const pi = Math.PI
+console.log(pi)
+
+const teto = Math.ceil(12.123) // arredonda para o numero mais proximo de cima
+console.log(teto)
+
+const chao = Math.floor(12.123) // arredonda o numero para baixo
+console.log(chao)
+
+const randon = Math.random()
+console.log(randon)
