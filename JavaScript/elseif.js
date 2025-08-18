c6ca5d25c914e59887fd31b4c986bd1c2ca5cf31
Nastsukii <lacb.lzn.luiz@gamil.com>
2025-08-18 19:04:16 -0300

    adição de elseif

---
+++ JavaScript/elseif.js
@@ -0,0 +1,11 @@
+const temperatura = 55
+
+    if(temperatura === 36){
+        console.log("Esta normal")
+    }else if (temperatura < 36){
+        console.log("esta com hipotermia")
+    }else{
+        console.log("esta com febre")
+    }
+
+     // ele permite usar varias condições
