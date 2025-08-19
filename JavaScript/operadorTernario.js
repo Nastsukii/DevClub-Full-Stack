44f08a5ac8a1131a9a2077985f51ec72f356527e
Nastsukii <lacb.lzn.luiz@gamil.com>
2025-08-18 21:01:53 -0300

    operador ternario

---
+++ JavaScript/operadorTernario.js
@@ -0,0 +1,29 @@
+    const salario = 3000
+
+    // if = ?
+    // else = :
+    // if (sem else) = &&
+    
+
+    if (salario < 3000){
+        console.log("é junior")
+    }else {
+        console.log("é pleno")
+    }
+
+    salario > 2000 && console.log("ë junior")
+
+
+    salario < 3000 ? console.log("é junior") : console.log("é pleno")
+
+    // vai ser um código que é pequeno
+
+    const temperatura = 55
+
+    if(temperatura === 36){
+        console.log("Esta normal")
+    }else if (temperatura < 36){
+        console.log("esta com hipotermia")
+    }else{
+        console.log("esta com febre")
+    }
