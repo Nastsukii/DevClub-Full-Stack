b3eff8268a2d2eb03375717b1d850d64e8ead9ab
Nastsukii <lacb.lzn.luiz@gamil.com>
2025-09-16 23:27:32 -0300

    Comentarios no FOREACH

--- Projetos/Javascript/InterfaceFiltrosHamburguer/script.js
+++ Projetos/Javascript/InterfaceFiltrosHamburguer/script.js
@@ -1,21 +1,22 @@
-const botaoForEach = document.getElementById('for-each')
-const lista = document.querySelector('ul')
+const botaoForEach = document.getElementById('for-each')  // aqui pega o botao do html
+const lista = document.querySelector('ul') // vai pegar a ul que esta vazia no html
 
-let myLi = ''
+let myLi = '' // vai criar um array de li vazia
 
-const mostrarTudo = () => {
+const mostrarTudo = () => { 
     
-    menuOptions.forEach(item => {
-        myLi = myLi + `
+    menuOptions.forEach(item => { // vai fazer passar de item por item no array menuOptions
+        myLi = myLi + // vai adicionar a li na lista de li vazia e sempre adicionando mais um para ir sempre uma li nova
+        ` 
      <li> 
            <img src=${item.src}>
            <p>${item.name}</p>
            <p>R$ ${item.price},00</p>
         </li>
     `
-        lista.innerHTML = myLi
+        lista.innerHTML = myLi // vai jogar toda a array de li para a tela
     })
     
 }
 
-botaoForEach.addEventListener('click', mostrarTudo)
+botaoForEach.addEventListener('click', mostrarTudo) // esse ele vai pegar o evento de click do botao e chamar a funcao
