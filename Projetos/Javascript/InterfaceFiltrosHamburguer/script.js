2878bd5e245c7f914714393b4531aae6d74a9423
Nastsukii <lacb.lzn.luiz@gamil.com>
2025-09-17 11:29:01 -0300

    Mapear para dar desconto finalizado

--- Projetos/Javascript/InterfaceFiltrosHamburguer/script.js
+++ Projetos/Javascript/InterfaceFiltrosHamburguer/script.js
@@ -1,22 +1,34 @@
 const botaoForEach = document.getElementById('for-each')  // aqui pega o botao do html
 const lista = document.querySelector('ul') // vai pegar a ul que esta vazia no html
+const botaoMapear = document.getElementById('map')
 
-let myLi = '' // vai criar um array de li vazia
 
-const mostrarTudo = () => { 
-    
-    menuOptions.forEach(item => { // vai fazer passar de item por item no array menuOptions
-        myLi = myLi + // vai adicionar a li na lista de li vazia e sempre adicionando mais um para ir sempre uma li nova
+
+const mostrarTudo = (produto) => {  // vai receber um valor que vai ser um array
+    let myLi = '' // vai criar um array de li vazia
+
+    produto.forEach(item => { // vai fazer passar de item por item no array recebido
+        myLi += // vai adicionar a li na lista de li vazia e sempre adicionando mais um para ir sempre uma li nova
         ` 
      <li> 
            <img src=${item.src}>
            <p>${item.name}</p>
-           <p>R$ ${item.price},00</p>
+           <p>R$ ${item.price}</p>
         </li>
     `
-        lista.innerHTML = myLi // vai jogar toda a array de li para a tela
     })
-    
+    lista.innerHTML = myLi // vai jogar toda a array de li para a tela
+
 }
 
-botaoForEach.addEventListener('click', mostrarTudo) // esse ele vai pegar o evento de click do botao e chamar a funcao
+const darDesconto = () => {
+    const newPrice = menuOptions.map(item => ({ // vai mapear o menuoptions e ja retornar o valor
+        ...item, // os ... coloca todos os itens na mesa para podermos utlizar apenas uma parte deles
+        price: item.price * 0.9 // vai dar os 10% de desconto e deixar o resto como está
+    }))
+    mostrarTudo(newPrice) // vai enviar os novos valores pro foreach
+}
+
+
+botaoForEach.addEventListener('click', () => mostrarTudo(menuOptions)) // esse ele vai pegar o evento de click do botao e chamar a funcao ja com um valor
+botaoMapear.addEventListener('click', darDesconto)
